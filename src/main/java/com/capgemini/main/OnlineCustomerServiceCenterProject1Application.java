14bab86c10ef6ab6bbf9a1aeabf8d441b53ec277
Tanushri-nanda <tanushrinandagopal@gmail.com>
2022-04-20 10:35:25 +0530

    commit 1

---
+++ src/main/java/com/capgemini/main/OnlineCustomerServiceCenterProject1Application.java
@@ -0,0 +1,15 @@
+package com.capgemini.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCustomerServiceCenterProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCustomerServiceCenterProject1Application.class, args);
+		System.out.println("Welcome");
+
+	}
+
+}
