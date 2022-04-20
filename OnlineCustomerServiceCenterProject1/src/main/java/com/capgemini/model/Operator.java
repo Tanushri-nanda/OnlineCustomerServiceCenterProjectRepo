05c1b248eab72901e0da3752f4753b7409577cbe
ADMIN <ADMIN@DESKTOP-V8BU7TK>
2022-04-20 17:43:19 +0530

    Committed

---
+++ OnlineCustomerServiceCenterProject1/src/main/java/com/capgemini/model/Operator.java
@@ -0,0 +1,12 @@
+package com.capgemini.model;
+
+public class Operator {
+	private int operatorId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String mobile;
+	private String city;
+	
+	private Department department;
+}
