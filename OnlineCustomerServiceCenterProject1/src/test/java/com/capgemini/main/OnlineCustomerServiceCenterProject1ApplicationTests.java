871f558e791438e2fe8d0dc2d2a1f60d0d34c9b1
Tanushri-nanda <103626267+Tanushri-nanda@users.noreply.github.com>
2022-04-20 11:06:23 +0530

    Add files via upload

---
+++ OnlineCustomerServiceCenterProject1/src/test/java/com/capgemini/main/OnlineCustomerServiceCenterProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCustomerServiceCenterProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
