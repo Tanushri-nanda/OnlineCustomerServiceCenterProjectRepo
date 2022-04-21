6436df3ac15a9fd2a827d33fef599a194fa526b1
ADMIN <ADMIN@DESKTOP-V8BU7TK>
2022-04-21 10:01:09 +0530

    Models committed

--- OnlineCustomerServiceCenterProject1/src/main/java/com/capgemini/model/Operator.java
+++ OnlineCustomerServiceCenterProject1/src/main/java/com/capgemini/model/Operator.java
@@ -9,4 +9,83 @@ public class Operator {
 	private String city;
 	
 	private Department department;
+
+	public Operator() {
+		super();
+	}
+
+	public Operator(int operatorId, String firstName, String lastName, String email, String mobile, String city,
+			Department department) {
+		super();
+		this.operatorId = operatorId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.mobile = mobile;
+		this.city = city;
+		this.department = department;
+	}
+
+	public int getOperatorId() {
+		return operatorId;
+	}
+
+	public void setOperatorId(int operatorId) {
+		this.operatorId = operatorId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		return "Operator [operatorId=" + operatorId + ", firstName=" + firstName + ", lastName=" + lastName + ", email="
+				+ email + ", mobile=" + mobile + ", city=" + city + ", department=" + department + "]";
+	}
+	
 }
